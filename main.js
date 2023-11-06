4618b382243ab367ad29ef0067f280a5a32bbcf5
MdShijanAli <freelancershijan@gmail.com>
2023-11-06 19:34:22 +0600

    all pages inclued header and footer

--- main.js
+++ main.js
@@ -1,3 +1,88 @@
+function dropdownHandler(element) {
+  let single = element.getElementsByTagName("ul")[0];
+  single.classList.toggle("hidden");
+}
+
+function MenuHandler(el, val) {
+  let MainList = el.parentElement.parentElement.getElementsByTagName("ul")[0];
+  let closeIcon =
+    el.parentElement.parentElement.getElementsByClassName("close-m-menu")[0];
+  let showIcon =
+    el.parentElement.parentElement.getElementsByClassName("show-m-menu")[0];
+  if (val) {
+    MainList.classList.remove("hidden");
+    el.classList.add("hidden");
+    closeIcon.classList.remove("hidden");
+  } else {
+    showIcon.classList.remove("hidden");
+    MainList.classList.add("hidden");
+    el.classList.add("hidden");
+  }
+}
+// ------------------------------------------------------
+let sideBar = document.getElementById("mobile-nav");
+let menu = document.getElementById("menu");
+let cross = document.getElementById("cross");
+const sidebarHandler = (check) => {
+  if (check) {
+    sideBar.style.transform = "translateX(0px)";
+    menu.classList.add("hidden");
+    cross.classList.remove("hidden");
+  } else {
+    sideBar.style.transform = "translateX(-100%)";
+    menu.classList.remove("hidden");
+    cross.classList.add("hidden");
+  }
+};
+let list = document.getElementById("list");
+let chevrondown = document.getElementById("chevrondown");
+let chevronup = document.getElementById("chevronup");
+const listHandler = (check) => {
+  if (check) {
+    list.classList.remove("hidden");
+    chevrondown.classList.remove("hidden");
+    chevronup.classList.add("hidden");
+  } else {
+    list.classList.add("hidden");
+    chevrondown.classList.add("hidden");
+    chevronup.classList.remove("hidden");
+  }
+};
+
+
+
+
+
+const parentMenu = document.querySelectorAll('.parent_menu');
+const childMenu = document.querySelectorAll('.child_menu');
+
+parentMenu.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    childMenu.forEach((content, contentIndex) => {
+      if (contentIndex === index) {
+        content.classList.toggle('show');
+      } else {
+        content.classList.remove('show');
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+console.log('hello');
+
+
+
+
+
+
+
+
 
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
@@ -31,8 +116,5 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].classList.add("bg-gray-700");
 }
-
-
-
 
 
